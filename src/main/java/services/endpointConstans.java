7996913b778a622194ac213bf169f4f10d04bb48
Gerant <geraskin.antony@yandex.ru>
2021-01-25 16:03:47 +0700

    Initial commit

---
+++ src/main/java/services/endpointConstans.java
@@ -0,0 +1,10 @@
+package services;
+
+public class endpointConstans {
+    //константы
+    protected static String URI ="https://jsonplaceholder.typicode.com";
+    protected static String COOKIE="";
+    protected static String JSON_DATA_PATH = "C:\\Users\\Gerant\\IdeaProjects" +
+            "\\Rest_API_Project_Test\\src\\main\\resources\\data.json";
+
+}
